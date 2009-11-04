a5fc5703c4cc0f93637773cdd35b61aac8544af8
Jonathan Anstey <janstey@gmail.com>
2009-11-04 00:38:19 +0000

    Initial project layout and start of some chapter 2 examples
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@2 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter2/cbr/src/main/java/camelinaction/Order.java
@@ -0,0 +1,36 @@
+package camelinaction;
+
+import java.io.Serializable;
+
+public class Order implements Serializable {
+    private String name;
+    private int amount;
+    
+    public Order() {
+    }
+    
+    public Order(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Order that = (Order) other;
+        return this.name.equals(that.name) && this.amount == that.amount;
+    }
+    
+    @Override
+    public String toString() {
+        return "Order[" + name + " , " + amount + "]";
+    }
+}
