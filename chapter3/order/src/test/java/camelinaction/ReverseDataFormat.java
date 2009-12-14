ee6f942bac06b110f4419da12320672a0e14a7f4
Claus Ibsen <claus.ibsen@gmail.com>
2009-12-14 19:48:25 +0000

    Chapter 3
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@31 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter3/order/src/test/java/camelinaction/ReverseDataFormat.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.spi.DataFormat;
+
+/**
+ * A simple custom data format that can reverse strings.
+ *
+ * @version $Revision$
+ */
+public class ReverseDataFormat implements DataFormat {
+
+    public void marshal(Exchange exchange, Object graph, OutputStream stream) throws Exception {
+        byte[] bytes = exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class, graph);
+        String body = reverseBytes(bytes);
+        stream.write(body.getBytes());
+    }
+
+    public Object unmarshal(Exchange exchange, InputStream stream) throws Exception {
+        byte[] bytes = exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class, stream);
+        String body = reverseBytes(bytes);
+        return body;
+    }
+
+    private String reverseBytes(byte[] data) {
+        StringBuilder sb = new StringBuilder(data.length);
+        for (int i = data.length - 1; i >= 0; i--) {
+            char ch = (char) data[i];
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+}
