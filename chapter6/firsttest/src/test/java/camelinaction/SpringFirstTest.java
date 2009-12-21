35246fa6b87d98678f786a742ee8598062522c8f
Claus Ibsen <claus.ibsen@gmail.com>
2009-12-21 09:51:44 +0000

    Chapter 6 first test
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@37 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter6/firsttest/src/test/java/camelinaction/SpringFirstTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import java.io.File;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Our first unit test with the Camel Test Kit testing Spring XML routes.
+ * We test the Hello World example of integration kits, which is copying a file.
+ *
+ * @version $Revision$
+ */
+public class SpringFirstTest extends CamelSpringTestSupport {
+
+    public void setUp() throws Exception {
+        // delete directories so we have a clean start
+        deleteDirectory("target/inbox");
+        deleteDirectory("target/outbox");
+        super.setUp();
+    }
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("camelinaction/firststep.xml");
+    }
+
+    @Test
+    public void testCopyFile() throws Exception {
+        // create a new file in the inbox folder with the name hello.txt and containing Hello World as body
+        template.sendBodyAndHeader("file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
+
+        // wait a while to let the file be moved
+        Thread.sleep(1000);
+
+        // test the file was copied
+        File target = new File("target/outbox/hello.txt");
+        assertTrue("File should have been copied", target.exists());
+
+        // test that its content is correct as well
+        String content = context.getTypeConverter().convertTo(String.class, target);
+        assertEquals("Hello World", content);
+    }
+
+}
