85b8a404348758d12c7aaaf0eb4b5812349187c7
Jonathan Anstey <janstey@gmail.com>
2010-03-04 01:29:31 +0000

    add examples for quartz component
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@74 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter7/quartz/src/main/java/camelinaction/QuartzExample.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class QuartzExample {
+
+    public static void main(String args[]) throws Exception {
+        // create CamelContext
+        CamelContext context = new DefaultCamelContext();
+
+        // add our route to the CamelContext
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("quartz://myTimer?trigger.repeatInterval=2000&trigger.repeatCount=-1")
+                .setBody().simple("I was fired at ${header.fireTime}")
+                .to("stream:out");
+            }
+        });
+
+        // start the route and let it do its work
+        context.start();
+        Thread.sleep(10000);
+
+        // stop the CamelContext
+        context.stop();
+    }
+    
+}
