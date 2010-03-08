829d5ae296b6c1c1625409c081789e5771e6077e
Claus Ibsen <claus.ibsen@gmail.com>
2010-03-08 12:04:56 +0000

    Chapter 9
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@83 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- chapter13/shutdown/src/test/java/camelinaction/GracefulShutdownBigFileXmlTest.java
+++ chapter13/shutdown/src/test/java/camelinaction/GracefulShutdownBigFileXmlTest.java
@@ -16,23 +16,17 @@
  */
 package camelinaction;
 
-import javax.sql.DataSource;
-
-import camelinaction.order.acme.InputOrder;
-import org.apache.camel.spi.Registry;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @version $Revision$
  */
-public class OrderDAO {
+public class GracefulShutdownBigFileXmlTest extends GracefulShutdownBigFileFailTest {
 
-    public void insertOrder(InputOrder order, Registry registry) {
-        DataSource ds = registry.lookup("myDataSource", DataSource.class);
-        JdbcTemplate jdbc = new JdbcTemplate(ds);
-
-        Object[] args = new Object[] { order.getCustomerId(), order.getRefNo(), order.getPartId(), order.getAmount()};
-        jdbc.update("insert into acme_order (customer_id, ref_no, part_id, amount) values (?, ?, ?, ?)", args);
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/camel-route-xml.xml");
     }
 
 }
---
+++ chapter9/multiple-routes/src/test/java/camelinaction/TXToTXTest.java
@@ -0,0 +1,88 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.SpringRouteBuilder;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @version $Revision$
+ */
+public class TXToTXTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring-context.xml");
+    }
+
+    @Override
+    protected int getExpectedRouteCount() {
+        // use 0 as we use a Java based route builder directly in this unit test
+        return 0;
+    }
+
+    @Test
+    public void testWithCamel() throws Exception {
+        template.sendBody("activemq:queue:a", "Hi Camel");
+
+        String reply = consumer.receiveBody("activemq:queue:b", 10000, String.class);
+        assertEquals("Hi Camel", reply);
+
+        reply = consumer.receiveBody("activemq:queue:camel", 10000, String.class);
+        assertEquals("Hi Camel", reply);
+    }
+
+    @Test
+    public void testWithDonkey() throws Exception {
+        template.sendBody("activemq:queue:a", "Donkey");
+
+        String reply = consumer.receiveBody("activemq:queue:b", 10000, String.class);
+        assertNull("There should be no reply", reply);
+
+        reply = consumer.receiveBody("activemq:queue:camel", 10000, String.class);
+        assertNull("There should be no reply", reply);
+
+        reply = consumer.receiveBody("activemq:queue:ActiveMQ.DLQ", 10000, String.class);
+        assertNotNull("It should have been moved to DLQ", reply);
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("activemq:queue:a")
+                    .transacted()
+                    .to("direct:quote")
+                    .to("activemq:queue:b");
+
+                from("direct:quote")
+                    .transacted()
+                    .choice()
+                        .when(body().contains("Camel"))
+                            .to("activemq:queue:camel")
+                    .otherwise()
+                        .throwException(new IllegalArgumentException("Unsupported animal"));
+            }
+        };
+    }
+
+}
