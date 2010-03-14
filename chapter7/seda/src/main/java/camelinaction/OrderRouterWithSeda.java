540bf3c40d8124aef770ff8168c49c1eb9b7f26b
Jonathan Anstey <janstey@gmail.com>
2010-03-14 17:27:16 +0000

    seda examples
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@90 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter7/seda/src/main/java/camelinaction/OrderRouterWithSeda.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * A set of routes that watches a directory for new orders, reads them, converts the order 
+ * file into a JMS Message and then sends it to the JMS incomingOrders queue hosted 
+ * on an embedded ActiveMQ broker instance.
+ * 
+ * From there a content-based router is used to send the order to either the
+ * xmlOrders or csvOrders queue.
+ *
+ * @author janstey
+ *
+ */
+public class OrderRouterWithSeda {
+
+    public static void main(String args[]) throws Exception {
+        // create CamelContext
+        CamelContext context = new DefaultCamelContext();
+        
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory = 
+            new ActiveMQConnectionFactory("vm://localhost");
+        context.addComponent("jms",
+            JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        // add our route to the CamelContext
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                // load file orders from src/data into the SEDA queue
+                from("file:src/data?noop=true").to("seda:incomingOrders");
+        
+                // content-based router
+                from("seda:incomingOrders")
+                .choice()
+                    .when(header("CamelFileName").endsWith(".xml"))
+                        .to("seda:xmlOrders?multipleConsumers=true")  
+                    .when(header("CamelFileName").endsWith(".csv"))
+                        .to("seda:csvOrders?multipleConsumers=true");
+
+                from("seda:xmlOrders?multipleConsumers=true").to("jms:accounting");  
+                from("seda:xmlOrders?multipleConsumers=true").to("jms:production");  
+                
+                // test that our route is working
+                from("jms:accounting").process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Accounting received order: "
+                                + exchange.getIn().getHeader("CamelFileName"));  
+                    }
+                });
+                from("jms:production").process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Production received order: "
+                                + exchange.getIn().getHeader("CamelFileName"));  
+                    }
+                });           
+            }
+        });
+
+        // start the route and let it do its work
+        context.start();
+        Thread.sleep(10000);
+
+        // stop the CamelContext
+        context.stop();
+    }
+}
