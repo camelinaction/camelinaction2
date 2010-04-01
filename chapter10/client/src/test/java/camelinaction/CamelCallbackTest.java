b61f185220c82e80a2ffba31161108214eca7f65
Claus Ibsen <claus.ibsen@gmail.com>
2010-04-01 13:26:52 +0000

    Chapter 10
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@106 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter10/client/src/test/java/camelinaction/CamelCallbackTest.java
@@ -0,0 +1,86 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.SynchronizationAdapter;
+import org.apache.camel.spi.Synchronization;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * A test which shows how to use the Camel callback mechanism to let the callback
+ * handle the replies when they are returned.
+ *
+ * @version $Revision$
+ */
+public class CamelCallbackTest extends CamelTestSupport {
+
+    @Test
+    public void testCamelCallback() throws Exception {
+        // echos is the list of replies
+        final List<String> echos = new ArrayList<String>();
+
+        // use this callback to gather the replies and add it to the echos list
+        Synchronization callback = new SynchronizationAdapter() {
+            @Override
+            public void onDone(Exchange exchange) {
+                // get the reply and add it to echoes
+                echos.add(exchange.getOut().getBody(String.class));
+            }
+        };
+
+        // now submit 3 async request/reply messages and use the same callback to
+        // handle the replies
+        template.asyncCallbackRequestBody("seda:echo", "Donkey", callback);
+        template.asyncCallbackRequestBody("seda:echo", "Tiger", callback);
+        template.asyncCallbackRequestBody("seda:echo", "Camel", callback);
+
+        // wait until the messages is done
+        Thread.sleep(5000);
+
+        // assert we got 3 replies
+        assertEquals(3, echos.size());
+        // sort list so we can assert by index
+        Collections.sort(echos);
+        assertEquals("CamelCamel", echos.get(0));
+        assertEquals("DonkeyDonkey", echos.get(1));
+        assertEquals("TigerTiger", echos.get(2));
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // this is the route we want to send messages to
+                // in an async manner.
+                // usually the route is something that takes some time to do
+                from("seda:echo?concurrentConsumers=5")
+                    .log("Starting to route ${body}")
+                    .delay(3000)
+                    .transform().simple("${body}${body}")
+                    .log("Route is now done");
+            }
+        };
+    }
+}
---
+++ chapter10/client/src/test/java/camelinaction/JavaFutureTest.java
@@ -0,0 +1,108 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import junit.framework.TestCase;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+/**
+ * Demonstrates how the client concurrency API in Java works
+ * for executing tasks in an asynchronous manner.
+ *
+ * @version $Revision$
+ */
+public class JavaFutureTest extends TestCase {
+
+    private static Log LOG = LogFactory.getLog(JavaFutureTest.class);
+
+    @Test
+    public void testFutureWithDone() throws Exception {
+        // this is the task we want to execute async
+        // usually the task is something that takes
+        // some time to do
+        Callable<String> task = new Callable<String>() {
+            public String call() throws Exception {
+                // do something that takes some time
+                LOG.info("Starting to process task");
+                Thread.sleep(5000);
+                LOG.info("Task is now done");
+                return "Camel rocks";
+            }
+        };
+
+        // this is the thread pool we will use
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        // now submit the task to the thread pool
+        // and get the Future handle back so we can later get the result
+        LOG.info("Submitting task to ExecutorService");
+        Future<String> future = executor.submit(task);
+        LOG.info("Task submitted and we got a Future handle");
+
+        // test when we are done
+        boolean done = false;
+        while (!done) {
+            done = future.isDone();
+            LOG.info("Is the task done? " + done);
+            if (!done) {
+                Thread.sleep(2000);
+            }
+        }
+
+        // and get the answer
+        String answer = future.get();
+        LOG.info("The answer is: " + answer);
+    }
+
+    @Test
+    public void testFutureWithoutDone() throws Exception {
+        // this is the task we want to execute async
+        // usually the task is something that takes
+        // some time to do
+        Callable<String> task = new Callable<String>() {
+            public String call() throws Exception {
+                // do something that takes some time
+                LOG.info("Starting to process task");
+                Thread.sleep(5000);
+                LOG.info("Task is now done");
+                return "Camel rocks";
+            }
+        };
+
+        // this is the thread pool we will use
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        // now submit the task to the thread pool
+        // and get the Future handle back so we can later get the result
+        LOG.info("Submitting task to ExecutorService");
+        Future<String> future = executor.submit(task);
+        LOG.info("Task submitted and we got a Future handle");
+
+        // instead of testing when we are done we can just get
+        // the result and it will automatic wait until the task is done
+        String answer = future.get();
+        LOG.info("The answer is: " + answer);
+    }
+
+}
