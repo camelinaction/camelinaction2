c7c322a6022ab475c86f0252cb091668e980d2ee
Claus Ibsen <claus.ibsen@gmail.com>
2010-04-18 08:42:49 +0000

    Chapter 8
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@117 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter8/eip/src/test/java/camelinaction/AggregateXMLTest.java
@@ -0,0 +1,74 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import static org.apache.camel.builder.xml.XPathBuilder.xpath;
+
+/**
+ * The XML example for using the Aggregator EIP.
+ * <p/>
+ * This example have XML messages send to the aggregator to demonstrate
+ * using XPath for correlation expression. And we have two completion
+ * conditions which is based on size and timeout
+ * <p/>
+ * See the class {@link MyAggregationStrategy} for how the messages
+ * are actually aggregated together.
+ *
+ * @see MyAggregationStrategy
+ * @version $Revision$
+ */
+public class AggregateXMLTest extends CamelTestSupport {
+
+    @Test
+    public void testXML() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(2);
+
+        template.sendBody("direct:start", "<order name=\"motor\" amount=\"1000\" customer=\"honda\"/>");
+        template.sendBody("direct:start", "<order name=\"motor\" amount=\"500\" customer=\"toyota\"/>");
+        template.sendBody("direct:start", "<order name=\"gearbox\" amount=\"200\" customer=\"toyota\"/>");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    .log("Sending ${body}")
+                    // aggregate based on xpath expression which extracts from the
+                    // arrived message body.
+                    // use class MyAggregationStrategy for aggregation
+                    .aggregate(xpath("/order/@customer"), new MyAggregationStrategy())
+                        // complete either when we have 2 messages or after 5 sec timeout
+                        .completionSize(2).completionTimeout(5000)
+                        // do a little logging for the published message
+                        .log("Completed by ${property.CamelAggregatedCompletedBy}")
+                        .log("Sending out ${body}")
+                        // and send it to the mock
+                        .to("mock:result");
+            }
+        };
+    }
+}
