93fb2b2a0a4a99e994c47e867e84de46202cfffd
Claus Ibsen <claus.ibsen@gmail.com>
2010-05-05 13:41:04 +0000

    Chapter 8
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@137 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter8/loadbalancer/src/test/java/camelinaction/SpringTopicLoadBalancerTest.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Demonstrates how to use the Load Balancer EIP pattern.
+ * <p/>
+ * Using the topic strategy.
+ *
+ * @version $Revision$
+ */
+public class SpringTopicLoadBalancerTest extends CamelSpringTestSupport {
+
+    @Test
+    public void testLoadBalancer() throws Exception {
+        // both mocks should get all the messages as its topic strategy
+        MockEndpoint a = getMockEndpoint("mock:a");
+        a.expectedMessageCount(4);
+
+        MockEndpoint b = getMockEndpoint("mock:b");
+        b.expectedMessageCount(4);
+
+        // send in 4 messages
+        template.sendBody("direct:start", "Hello");
+        template.sendBody("direct:start", "Camel rocks");
+        template.sendBody("direct:start", "Cool");
+        template.sendBody("direct:start", "Bye");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/topic-loadbalancer.xml");
+    }
+}
