da2c4b3300c5f8a678577acd14057bc787831336
Claus Ibsen <claus.ibsen@gmail.com>
2010-05-08 15:18:45 +0000

    Chapter 8
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@141 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter8/routingslip/src/test/java/camelinaction/camelinaction/SpringRoutingSlipSimpleTest.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction.camelinaction;
+
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * A basic example how to use Routing Slip EIP.
+ * <p/>
+ * This example is a simple example how to route a message using the routing slip EIP
+ * based on an existing header which dictates the sequence steps.
+ *
+ * @version $Revision$
+ */
+public class SpringRoutingSlipSimpleTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/routingslip-simple.xml");
+    }
+
+    @Test
+    public void testRoutingSlip() throws Exception {
+        // setup expectations that only A and C will receive the message
+        getMockEndpoint("mock:a").expectedMessageCount(1);
+        getMockEndpoint("mock:b").expectedMessageCount(0);
+        getMockEndpoint("mock:c").expectedMessageCount(1);
+
+        // send the incoming message with the attached slip
+        template.sendBodyAndHeader("direct:start", "Hello World", "mySlip", "mock:a,mock:c");
+
+        assertMockEndpointsSatisfied();
+    }
+
+}
---
+++ chapter8/routingslip/src/test/java/camelinaction/camelinaction/SpringRoutingSlipTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction.camelinaction;
+
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * An example how to use Routing Slip EIP.
+ * <p/>
+ * This example uses a bean to compute the initial routing slip which must
+ * be provided as a header to the Routing Slip EIP.
+ *
+ * @version $Revision$
+ */
+public class SpringRoutingSlipTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/routingslip.xml");
+    }
+
+    @Test
+    public void testRoutingSlip() throws Exception {
+        // setup expectations that only A and C will receive the message
+        getMockEndpoint("mock:a").expectedMessageCount(1);
+        getMockEndpoint("mock:b").expectedMessageCount(0);
+        getMockEndpoint("mock:c").expectedMessageCount(1);
+
+        // send the incoming message
+        template.sendBody("direct:start", "Hello World");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testRoutingSlipCool() throws Exception {
+        // setup expectations that all will receive the message
+        getMockEndpoint("mock:a").expectedMessageCount(1);
+        getMockEndpoint("mock:b").expectedMessageCount(1);
+        getMockEndpoint("mock:c").expectedMessageCount(1);
+
+        template.sendBody("direct:start", "We are Cool");
+
+        assertMockEndpointsSatisfied();
+    }
+
+}
