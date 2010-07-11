f803d14fceec0fef47bda72337273d5d7970a299
Claus Ibsen <claus.ibsen@gmail.com>
2010-07-11 12:12:27 +0000

    Chapter 5
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@181 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- chapter5/errorhandler/src/test/java/camelinaction/OrderService.java
+++ chapter5/errorhandler/src/test/java/camelinaction/OrderService.java
@@ -16,6 +16,11 @@
  */
 package camelinaction;
 
+import java.io.InputStream;
+
+import org.apache.camel.Exchange;
+import org.w3c.dom.Document;
+
 /**
  * @version $Revision$
  */
@@ -46,5 +51,25 @@ public class OrderService {
         }
 
         return body.replaceAll("#", ",");
+    }
+
+    public void toSoap(Exchange exchange) {
+        String body = exchange.getIn().getBody(String.class);
+        if (body.contains("ActiveMQ in Action")) {
+            // load the soapFault.xml into a DOM
+            InputStream is = exchange.getContext().getClassResolver().loadResourceAsStream("camelinaction/soapFault.xml");
+            Document dom = exchange.getContext().getTypeConverter().convertTo(Document.class, is);
+
+            // set a fault to indicate a failure
+            exchange.getOut().setFault(true);
+            exchange.getOut().setBody(dom);
+        } else {
+            // load the soapOK.xml into a DOM
+            InputStream is = exchange.getContext().getClassResolver().loadResourceAsStream("camelinaction/soapOk.xml");
+            Document dom = exchange.getContext().getTypeConverter().convertTo(Document.class, is);
+
+            // set a xml reply
+            exchange.getOut().setBody(dom);
+        }
     }
 }
