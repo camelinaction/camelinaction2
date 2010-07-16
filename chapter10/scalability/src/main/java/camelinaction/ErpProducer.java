5d4d3a56c1a3a847e3385a2c0794f02935257bf9
Claus Ibsen <claus.ibsen@gmail.com>
2010-07-16 08:58:22 +0000

    Chapter 10
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@190 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter10/scalability/src/main/java/camelinaction/ErpProducer.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import java.util.concurrent.ExecutorService;
+
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultAsyncProducer;
+
+/**
+ * Producer to simulate asynchronous communication with ERP system.
+
+ * @version $Revision$
+ */
+public class ErpProducer extends DefaultAsyncProducer {
+
+    // use a thread pool for async communication with ERP
+    private ExecutorService executor;
+
+    public ErpProducer(Endpoint endpoint) {
+        super(endpoint);
+        // use Camel to create the thread pool for us
+        this.executor = endpoint.getCamelContext().getExecutorServiceStrategy().newFixedThreadPool(this, "ERP", 10);
+    }
+
+    public boolean process(final Exchange exchange, final AsyncCallback callback) {
+        // simulate async communication using a thread pool in which will return a reply in 5 seconds.
+        executor.submit(new Runnable() {
+            public void run() {
+                log.info("Calling ERP");
+                // simulate communication with ERP takes 5 seconds
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+                log.info("ERP reply received");
+
+                // set reply
+                String in = exchange.getIn().getBody(String.class);
+                exchange.getOut().setBody(in + ";516");
+
+                // notify callback we are done
+                // must must use done(false) because this method returned false
+                log.info("Continue routing");
+                callback.done(false);
+            }
+        });
+
+        // return false to tell Camel that we process asynchronously
+        // which enables the Camel routing engine to know this and act accordingly
+        return false;
+    }
+}
---
+++ chapter10/scalability/src/test/java/camelinaction/ScalabilityTest.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * An example using asynchronous processing.
+ * <p/>
+ * Both Jetty and the ERP component supports async processing which means they offer better
+ * scalability as threads are not blocked.
+ * <p/>
+ * Run the two test methods (sync vs. async) and compare the threads being used when logging the input and output.
+ *
+ * @version $Revision$
+ */
+public class ScalabilityTest extends CamelTestSupport {
+
+    @Test
+    public void testSync() throws Exception {
+        String reply = template.requestBody("http://localhost:8090/webshop/action/search", "bumper", String.class);
+        assertEquals("Some other action here", reply);
+
+        // when running this test, notice the threads being used to log the input/output
+        // in the sync case its the same thread
+    }
+
+    @Test
+    public void testAsync() throws Exception {
+        // a customer requests the pricing for the buying 4 bumpers (id = 1719)
+        String reply = template.requestBody("http://localhost:8090/webshop/action/pricing", "1234;4;1719;bumper", String.class);
+        // the reply comes back with the price last = 516
+        assertEquals("1234;4;1719;bumper;516", reply);
+
+        // when running this test, notice the threads being used to log the input/output
+        // in the async case its different threads
+    }
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext context = super.createCamelContext();
+
+        // add the ERP component
+        context.addComponent("erp", new ErpComponent());
+
+        return context;
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("jetty:http://localhost:8090/webshop?matchOnUriPrefix=true")
+                    // log input
+                    .to("log:input")
+                    .choice()
+                        .when(header(Exchange.HTTP_PATH).isEqualTo("/webshop/action/pricing"))
+                            // communicate with ERP to calculate pricing
+                            .to("erp:pricing")
+                        .otherwise()
+                            // other actions
+                            .to("direct:other")
+                    .end()
+                    // log output
+                    .to("log:output");
+
+                // just return a fixed reply for other actions
+                from("direct:other")
+                    .transform(constant("Some other action here"));
+            }
+        };
+    }
+
+}
