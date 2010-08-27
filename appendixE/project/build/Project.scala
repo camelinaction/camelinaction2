9d4b9371318573cd83242642a29493507a64f43d
Martin Krasser <krasserm@apache.org>
2010-08-27 09:41:59 +0000

    Configuration files for Akka and loggers added
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@204 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- appendixE/project/build/Project.scala
+++ appendixE/project/build/Project.scala
@@ -6,6 +6,10 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val akkaSpring = "se.scalablesolutions.akka" % "akka-spring_2.8.0" % "0.10" % "compile"
   val camelJetty = "org.apache.camel" % "camel-jetty" % "2.4.0" % "compile"
   val camelSpring = "org.apache.camel" % "camel-spring" % "2.4.0" % "compile"
+  val log4j = "log4j" % "log4j" % "1.2.14" % "runtime"
 
   override def repositories = Set("akka" at "http://scalablesolutions.se/akka/repository")
+
+  // Temporary workaround to ignore logback.xml in akka-core_2.8.0-0.10.jar
+  System.setProperty("logback.configurationFile", "src/main/resources/logback.xml")
 }
