deb25bc629815f09f2c4ecb95642867b7fdbc7e3
Martin Krasser <krasserm@apache.org>
2010-08-27 05:27:55 +0000

    Build now based on Akka 0.10 jars from the Akka Maven repository.
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@202 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- appendixE/project/build/Project.scala
+++ appendixE/project/build/Project.scala
@@ -1,15 +1,11 @@
 import sbt._
 import Process._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
-  val akkaCamel = akkaModule("camel")
-  val akkaSpring = akkaModule("spring")
-  
+class Project(info: ProjectInfo) extends DefaultProject(info) {
+  val akkaCamel = "se.scalablesolutions.akka" % "akka-camel_2.8.0" % "0.10" % "compile"
+  val akkaSpring = "se.scalablesolutions.akka" % "akka-spring_2.8.0" % "0.10" % "compile"
   val camelJetty = "org.apache.camel" % "camel-jetty" % "2.4.0" % "compile"
   val camelSpring = "org.apache.camel" % "camel-spring" % "2.4.0" % "compile"
-  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.0" % "compile"
-  val junit = "junit" % "junit" % "4.8.1" % "test->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test->default"
 
-  override def repositories = Set( "scala-tools-snapshots" at "http://scala-tools.org/repo-snapshots/")
+  override def repositories = Set("akka" at "http://scalablesolutions.se/akka/repository")
 }
