eef9283c886cb04867238eca57afb1e25a71b3ec
Martin Krasser <krasserm@apache.org>
2010-09-08 19:18:54 +0000

    Use log4j-over-slf4j to avoid log config redundancies
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@235 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- appendixE/project/build/Project.scala
+++ appendixE/project/build/Project.scala
@@ -6,8 +6,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
 
   val camelJetty = "org.apache.camel" % "camel-jetty" % "2.4.0" % "compile"
   val camelSpring = "org.apache.camel" % "camel-spring" % "2.4.0" % "compile"
-
-  val log4j = "log4j" % "log4j" % "1.2.14" % "runtime"
+  val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.6.0" % "runtime"
 
   // Temporary workaround to ignore logback.xml in akka-core_2.8.0-0.10.jar
   System.setProperty("logback.configurationFile", "src/main/resources/logback.xml")
