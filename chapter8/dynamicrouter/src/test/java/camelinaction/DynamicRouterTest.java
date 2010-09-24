0e6c38f518ee92765e1c49fa418cedc626567ef0
Claus Ibsen <claus.ibsen@gmail.com>
2010-09-24 13:05:22 +0000

    Chapter 8
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@249 2ef4527c-bcc6-11de-8784-f7ad4773dbab

---
+++ chapter8/dynamicrouter/src/test/java/camelinaction/DynamicRouterTest.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * Dynamic Router EIP example
+ *
+ * @version $Revision$
+ */
+public class DynamicRouterTest extends CamelTestSupport {
+
+    @Test
+    public void testDynamicRouter() throws Exception {
+        getMockEndpoint("mock:a").expectedBodiesReceived("Camel");
+        getMockEndpoint("mock:result").expectedBodiesReceived("Bye Camel");
+
+        template.sendBody("direct:start", "Camel");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    // use Dynamic Router EIP to route message dynamically
+                    // use the DynamicRouterBean bean which provides the logic
+                    // to compute where the message should go
+                    .dynamicRouter(bean(DynamicRouterBean.class, "route"))
+                    // when we are done with the Dynamic Router EIP go to
+                    // the mock result endpoint
+                    .to("mock:result");
+            }
+        };
+    }
+}
