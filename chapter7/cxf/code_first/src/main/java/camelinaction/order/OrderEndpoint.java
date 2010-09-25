fca1a8499790ddf0f384ebfc4d32ddcbaaac6cc1
Jonathan Anstey <janstey@gmail.com>
2010-09-25 18:30:44 +0000

    code cleanup
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@253 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- chapter7/cxf/code_first/src/main/java/camelinaction/order/OrderEndpoint.java
+++ chapter7/cxf/code_first/src/main/java/camelinaction/order/OrderEndpoint.java
@@ -4,5 +4,5 @@ import javax.jws.WebService;
 
 @WebService
 public interface OrderEndpoint {
-    public String order(String partName, int amount, String customerName);
+    String order(String partName, int amount, String customerName);
 }
