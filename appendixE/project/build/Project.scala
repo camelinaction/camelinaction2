390ab1617cc6a550c8160e6fa113b19576d4fddc
Martin Krasser <krasserm@apache.org>
2010-10-14 09:41:42 +0000

    Upgraded and tested appendixE with Camel 2.5-SNAPSHOT. Will be changed to Camel 2.5.0 as soon as it is released.
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@276 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- appendixE/project/build/Project.scala
+++ appendixE/project/build/Project.scala
@@ -1,11 +1,14 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
+  // Temporary (remove when Camel 2.5.0 is released)
+  val apacheSnapshots = MavenRepository("apache-snapshots", "https://repository.apache.org/content/repositories/snapshots/")
+
   val akkaCamel = akkaModule("camel")
   val akkaKernel = akkaModule("spring")
 
-  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5.0" % "compile"
-  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5.0" % "compile"
+  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5-SNAPSHOT" % "compile"
+  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5-SNAPSHOT" % "compile"
   val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.6.1" % "runtime"
 
   // Temporary workaround to ignore logback.xml in akka-core_2.8.0-0.10.jar
