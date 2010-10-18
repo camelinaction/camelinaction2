141ae77106c17a3ddf5c46d934fdbdcdca6c8afd
Martin Krasser <krasserm@apache.org>
2010-10-18 14:40:27 +0000

    Upgraded to Camel 2.5 release candidate
    git-svn-id: http://camelinaction.googlecode.com/svn/trunk@281 2ef4527c-bcc6-11de-8784-f7ad4773dbab

--- appendixE/project/build/Project.scala
+++ appendixE/project/build/Project.scala
@@ -2,13 +2,13 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
   // Temporary (remove when Camel 2.5.0 is released)
-  val apacheSnapshots = MavenRepository("apache-snapshots", "https://repository.apache.org/content/repositories/snapshots/")
+  val camelStaging = MavenRepository("camel-staging", "https://repository.apache.org/content/repositories/orgapachecamel-001/")
 
   val akkaCamel = akkaModule("camel")
   val akkaKernel = akkaModule("spring")
 
-  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5-SNAPSHOT" % "compile"
-  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5-SNAPSHOT" % "compile"
+  val camelJetty = "org.apache.camel" % "camel-jetty" % "2.5.0" % "compile"
+  val camelSpring = "org.apache.camel" % "camel-spring" % "2.5.0" % "compile"
   val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.6.1" % "runtime"
 
   // Temporary workaround to ignore logback.xml in akka-core_2.8.0-0.10.jar
