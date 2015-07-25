72368a6bc9c1218df76600e981fab553460dc979
Jonathan Anstey <janstey@gmail.com>
2015-07-24 23:23:43 -0230

    add some more spring tests

--- chapter14/payload/src/test/java/camelinaction/SpringMessageEncryptionTest.java
+++ chapter14/payload/src/test/java/camelinaction/SpringMessageEncryptionTest.java
@@ -20,33 +20,30 @@ import java.security.Key;
 import java.security.KeyStore;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.converter.crypto.CryptoDataFormat;
-import org.apache.camel.impl.JndiRegistry;
-import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.apache.camel.util.jsse.KeyStoreParameters;
 import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MessageEncryptionTest extends CamelTestSupport {
+public class SpringMessageEncryptionTest extends CamelSpringTestSupport {
 
-    private Key secretKey;
-
-    @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry registry = super.createRegistry();
-        
+    // TODO remove this once keystoreparam support added to crypto dataformat
+    public static Key loadKey() throws Exception {
         KeyStoreParameters keystore = new KeyStoreParameters();
         keystore.setPassword("supersecret");
         keystore.setResource("./cia_secrets.jceks");
         keystore.setType("JCEKS");
         
         KeyStore store = keystore.createKeyStore();
-        secretKey = store.getKey("ciasecrets", "secret".toCharArray());
-        registry.bind("secretKey", secretKey);
-        return registry;
+        return store.getKey("ciasecrets", "secret".toCharArray());
+    }
+    
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/message-encryption.xml"});
     }
 
-        
     @Test
     public void testEncryptAndDecryptMessage() throws Exception {
         getMockEndpoint("mock:unencrypted").expectedBodiesReceived("Hello World");
@@ -57,21 +54,5 @@ public class MessageEncryptionTest extends CamelTestSupport {
         
         Exchange exchange = getMockEndpoint("mock:encrypted").getReceivedExchanges().get(0);
         assertNotEquals("Hello World", exchange.getIn().getBody());
-    }
-
-    @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
-        return new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                CryptoDataFormat crypto = new CryptoDataFormat("DES", secretKey);
-                
-                from("direct:start")
-                .marshal(crypto)
-                .to("mock:encrypted")
-                .unmarshal(crypto)
-                .to("mock:unencrypted");
-            }
-        };
     }
 }
