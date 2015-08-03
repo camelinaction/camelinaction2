fb2af88290050a72dc0a9442d0cce5b4899c439a
Claus Ibsen <claus.ibsen@gmail.com>
2015-08-03 10:09:16 +0200

    Chapter 13

--- chapter13/scalability/src/test/java/camelinaction/timeout/TimeoutComponent.java
+++ chapter13/scalability/src/test/java/camelinaction/timeout/TimeoutComponent.java
@@ -3,12 +3,18 @@ package camelinaction.timeout;
 import java.util.Map;
 
 import org.apache.camel.Endpoint;
-import org.apache.camel.impl.DefaultComponent;
+import org.apache.camel.impl.UriEndpointComponent;
 
-public class TimeoutComponent extends DefaultComponent {
+public class TimeoutComponent extends UriEndpointComponent {
+
+    public TimeoutComponent() {
+        super(TimeoutEndpoint.class);
+    }
 
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        return new TimeoutEndpoint(uri, this);
+        TimeoutEndpoint answer = new TimeoutEndpoint(uri, this);
+        answer.setName(remaining);
+        return answer;
     }
 }
--- chapter13/scalability/src/test/java/camelinaction/timeout/TimeoutEndpoint.java
+++ chapter13/scalability/src/test/java/camelinaction/timeout/TimeoutEndpoint.java
@@ -5,9 +5,17 @@ import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.spi.UriPath;
 
+@UriEndpoint(scheme = "timeout", title = "Timeout", syntax = "timeout:name", producerOnly = true, label = "custom")
 public class TimeoutEndpoint extends DefaultEndpoint {
 
+    @UriPath
+    private String name;
+
+    @UriParam(defaultValue = "10000")
     private int timeout = 10000;
 
     public TimeoutEndpoint(String endpointUri, Component component) {
@@ -29,10 +37,24 @@ public class TimeoutEndpoint extends DefaultEndpoint {
         return true;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Logical name of endpoint.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getTimeout() {
         return timeout;
     }
 
+    /**
+     * Timeout in millis
+     */
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
