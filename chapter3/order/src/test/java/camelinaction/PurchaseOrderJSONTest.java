b39411cb106197605e522820bca0211c493c76fd
Claus Ibsen <claus.ibsen@gmail.com>
2015-10-02 08:16:00 +0200

    Polished

--- chapter3/order/src/test/java/camelinaction/PurchaseOrderJSONTest.java
+++ chapter3/order/src/test/java/camelinaction/PurchaseOrderJSONTest.java
@@ -1,19 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package camelinaction;
 
 import org.apache.camel.Header;
@@ -29,8 +13,6 @@ import org.slf4j.LoggerFactory;
  * Example how to use JSON data format with the camel-jackson component.
  * <p/>
  * We use camel-jetty to expose a HTTP service which returns the JSON response.
- *
- * @version $Revision$
  */
 public class PurchaseOrderJSONTest extends CamelTestSupport {
 
@@ -46,7 +28,7 @@ public class PurchaseOrderJSONTest extends CamelTestSupport {
 
     @Test
     public void testJSON() throws Exception {
-        String out = template.requestBody("http://localhost:8080/order/service?id=123", null, String.class);
+        String out = template.requestBody("jetty:http://localhost:8080/order/service?id=123", null, String.class);
         LOG.info("Response from order service: " + out);
 
         assertNotNull(out);
