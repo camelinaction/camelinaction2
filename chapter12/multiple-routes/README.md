65317166cfbdb30b1a09ab0782f9808479f51664
Claus Ibsen <claus.ibsen@gmail.com>
2015-11-08 14:28:14 +0100

    Chapter 12 - spring version of the example

--- chapter12/multiple-routes/README.md
+++ chapter12/multiple-routes/README.md
@@ -8,10 +8,12 @@ This directory holds an example how to use transactions across multiple Camel ro
 The example can be run with:
 
     mvn test -Dtest=TXToNonTXTest
+    mvn test -Dtest=SpringTXToNonTXTest
 
 ### 12.3.4 - Using transactions with another transacted route
 
 The example can be run with:
 
     mvn test -Dtest=TXToTXTest
+    mvn test -Dtest=SpringTXToTXTest
 
--- chapter12/multiple-routes/src/test/java/camelinaction/SpringTXToNonTXTest.java
+++ chapter12/multiple-routes/src/test/java/camelinaction/SpringTXToNonTXTest.java
@@ -1,17 +1,15 @@
 package camelinaction;
 
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.SpringRouteBuilder;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TXToNonTXTest extends CamelSpringTestSupport {
+public class SpringTXToNonTXTest extends CamelSpringTestSupport {
 
     @Override
     protected AbstractApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext("spring-context.xml");
+        return new ClassPathXmlApplicationContext("TXToNonTXTest.xml");
     }
 
     @Test
@@ -39,28 +37,6 @@ public class TXToNonTXTest extends CamelSpringTestSupport {
 
         reply = consumer.receiveBody("activemq:queue:ActiveMQ.DLQ", 10000, String.class);
         assertNotNull("It should have been moved to DLQ", reply);
-    }
-
-    @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
-        return new SpringRouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("activemq:queue:a")
-                    .transacted()
-                    .to("direct:quote")
-                    .to("activemq:queue:b");
-
-                from("direct:quote")
-                    .choice()
-                        .when(body().contains("Camel"))
-                            .transform(constant("Camel rocks"))
-                        .when(body().contains("Donkey"))
-                            .throwException(new IllegalArgumentException("Donkeys not allowed"))
-                    .otherwise()
-                        .transform(body().prepend("Hello "));
-            }
-        };
     }
 
 }
