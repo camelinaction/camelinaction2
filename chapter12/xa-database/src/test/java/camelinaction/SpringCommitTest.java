17e83a0f2968d9c5d213194ccc51cc8eea4ec19a
Claus Ibsen <claus.ibsen@gmail.com>
2015-11-18 19:47:06 +0100

    Chapter 12 - XA database example

---
+++ chapter12/xa-database/src/test/java/camelinaction/SpringCommitTest.java
@@ -0,0 +1,58 @@
+package camelinaction;
+
+import java.util.concurrent.TimeUnit;
+import javax.sql.DataSource;
+
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class SpringCommitTest extends CamelSpringTestSupport {
+
+    private JdbcTemplate jdbc;
+
+    @Before
+    public void setupDatabase() throws Exception {
+        DataSource ds = context.getRegistry().lookupByNameAndType("myDataSource", DataSource.class);
+        jdbc = new JdbcTemplate(ds);
+
+        jdbc.execute("create table partner_metric "
+                + "( partner_id varchar(10), time_occurred varchar(20), status_code varchar(3), perf_time varchar(10) )");
+    }
+
+    @After
+    public void dropDatabase() throws Exception {
+        jdbc.execute("drop table partner_metric");
+    }
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("SpringCommitTest.xml");
+    }
+
+    @Test
+    public void testCommit() throws Exception {
+        NotifyBuilder notify = new NotifyBuilder(context).whenDone(1).create();
+
+        jdbc.execute("insert into partner_metric (partner_id, time_occurred, status_code, perf_time) values ('123', '20151115183457', '200', '1503')");
+
+        assertTrue(notify.matches(10, TimeUnit.SECONDS));
+
+        // give time for database
+        Thread.sleep(1000);
+
+        // and there should be 0 rows in the database
+        assertEquals(0, jdbc.queryForInt("select count(*) from partner_metric"));
+
+        String order = consumer.receiveBody("activemq:queue:order", 2000, String.class);
+        assertNotNull("Should be in order queue", order);
+
+        context.stop();
+    }
+
+}
---
+++ chapter12/xa-database/src/test/java/camelinaction/SpringRollbackAfterActiveMQTest.java
@@ -0,0 +1,55 @@
+package camelinaction;
+
+import java.util.concurrent.TimeUnit;
+import javax.sql.DataSource;
+
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class SpringRollbackAfterActiveMQTest extends CamelSpringTestSupport {
+
+    private JdbcTemplate jdbc;
+
+    @Before
+    public void setupDatabase() throws Exception {
+        DataSource ds = context.getRegistry().lookupByNameAndType("myDataSource", DataSource.class);
+        jdbc = new JdbcTemplate(ds);
+
+        jdbc.execute("create table partner_metric "
+                + "( partner_id varchar(10), time_occurred varchar(20), status_code varchar(3), perf_time varchar(10) )");
+    }
+
+    @After
+    public void dropDatabase() throws Exception {
+        jdbc.execute("drop table partner_metric");
+    }
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("SpringRollbackBeforeActiveMQTest.xml");
+    }
+
+    @Test
+    public void testRollbackAfterActiveMQ() throws Exception {
+        NotifyBuilder notify = new NotifyBuilder(context).whenReceived(10).create();
+
+        jdbc.execute("insert into partner_metric (partner_id, time_occurred, status_code, perf_time) values ('123', '20151115183457', '200', '1503')");
+
+        assertTrue(notify.matches(15, TimeUnit.SECONDS));
+
+        // and there should be 1 row in the database as it keep rolling back
+        assertEquals(1, jdbc.queryForInt("select count(*) from partner_metric"));
+
+        String order = consumer.receiveBody("activemq:queue:order", 2000, String.class);
+        assertNull("Should NOT be in order queue", order);
+
+        context.stop();
+    }
+
+}
