39604bdb9287662a6b34115acfb7c8b3099c73ee
Claus Ibsen <claus.ibsen@gmail.com>
2016-04-09 17:15:20 +0200

    Chapter 9

--- chapter9/advicewith/src/test/java/camelinaction/ReplaceFromTest.java
+++ chapter9/advicewith/src/test/java/camelinaction/ReplaceFromTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 /**
  * Unit test demonstrating various functionality of using advice-with
  */
-public class AdviceWithTest extends CamelTestSupport {
+public class ReplaceFromTest extends CamelTestSupport {
 
     @Override
     public boolean isUseAdviceWith() {
@@ -19,24 +19,28 @@ public class AdviceWithTest extends CamelTestSupport {
     }
 
     @Test
-    public void testMockEndpoints() throws Exception {
+    public void testReplaceFromWithEndpoints() throws Exception {
         RouteDefinition route = context.getRouteDefinition("quotes");
         route.adviceWith(context, new AdviceWithRouteBuilder() {
             @Override
             public void configure() throws Exception {
-                mockEndpoints();
+                // replace the incoming endpoint with a direct endpoint
+                // we can easily call from unit test
+                replaceFromWith("direct:hitme");
+                // and then mock all seda endpoints os we can use mock endpoints
+                // to assert the test is correct
+                mockEndpoints("seda:*");
             }
         });
 
         // must start Camel after we are done using advice-with
         context.start();
 
-
         getMockEndpoint("mock:seda:camel").expectedBodiesReceived("Camel rocks");
         getMockEndpoint("mock:seda:other").expectedBodiesReceived("Bad donkey");
 
-        template.sendBody("seda:quotes", "Camel rocks");
-        template.sendBody("seda:quotes", "Bad donkey");
+        template.sendBody("direct:hitme", "Camel rocks");
+        template.sendBody("direct:hitme", "Bad donkey");
 
         assertMockEndpointsSatisfied();
     }
@@ -46,7 +50,7 @@ public class AdviceWithTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("seda:quotes").routeId("quotes")
+                from("file:inbox").routeId("quotes")
                     .choice()
                         .when(simple("${body} contains 'Camel'"))
                             .to("seda:camel")
