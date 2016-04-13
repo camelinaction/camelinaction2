e7e475e791805454eb7ffb454bf8adf9a5681f5e
Jonathan Anstey <janstey@gmail.com>
2016-04-12 22:14:55 -0230

    Add cbr with simple testcase

---
+++ chapter2/cbr/src/test/java/camelinaction/OrderRouterSimpleTest.java
@@ -0,0 +1,81 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class OrderRouterSimpleTest extends CamelTestSupport {
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        // create CamelContext
+        CamelContext camelContext = super.createCamelContext();
+        
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory = 
+            new ActiveMQConnectionFactory("vm://localhost");
+        camelContext.addComponent("jms",
+            JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        
+        return camelContext;
+    }
+    
+    @Test
+    @Ignore("TODO enable when we update to Camel 2.17.1 or 2.18")
+    public void testPlacingOrders() throws Exception {
+        getMockEndpoint("mock:xml").expectedMessageCount(1);
+        getMockEndpoint("mock:csv").expectedMessageCount(1);
+
+        assertMockEndpointsSatisfied();
+    }
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // load file orders from src/data into the JMS queue
+                from("file:src/data?noop=true").to("jms:incomingOrders");
+        
+                // content-based router
+                from("jms:incomingOrders")
+                .choice()
+                    .when(simple("${header.CamelFileName} ends with 'xml'"))
+                        .to("jms:xmlOrders")  
+                    .when(simple("${header.CamelFileName} ends with 'csv'"))
+                        .to("jms:csvOrders");
+                
+                // test that our route is working
+                from("jms:xmlOrders")
+                .log("Received XML order: ${header.CamelFileName}")
+                .to("mock:xml");                
+                
+                from("jms:csvOrders")
+                .log("Received CSV order: ${header.CamelFileName}")
+                .to("mock:csv");
+            }
+        };
+    }
+}
