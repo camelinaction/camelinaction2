3463bf6160a7432ee83bad5728b10cfd32d7c919
Jonathan Anstey <janstey@gmail.com>
2016-04-17 16:45:49 -0230

    spring multicast tests

---
+++ chapter2/multicast/src/test/java/camelinaction/SpringOrderRouterWithMulticastTest.java
@@ -0,0 +1,21 @@
+package camelinaction;
+
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringOrderRouterWithMulticastTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("camelinaction/SpringOrderRouterWithMulticastTest.xml");
+    }
+
+    @Test
+    public void testPlacingOrders() throws Exception {
+        getMockEndpoint("mock:accounting").expectedMessageCount(1);
+        getMockEndpoint("mock:production").expectedMessageCount(1);
+        assertMockEndpointsSatisfied();
+    }
+}
