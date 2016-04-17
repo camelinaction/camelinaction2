9dfefb8554e7bd1d1326edd50224be2493aae05c
Claus Ibsen <claus.ibsen@gmail.com>
2016-04-17 21:03:32 +0200

    Chapter 9

--- chapter9/spring-xml/ReadMe.md
+++ chapter9/spring-xml/ReadMe.md
@@ -5,7 +5,7 @@ How to get started using the `camel-test-spring` module for testing Camel routes
 
 ### 9.2.1 Camel testing with Spring XML
 
-You can try this example by running this goal:
+You can try this example by running these Maven commands:
 
     mvn test -Dtest=SpringFirstTest
-
+    mvn test -Dtest=SpringFirstRunWithTest
---
+++ chapter9/spring-xml/src/test/java/camelinaction/SpringFirstRunWithTest.java
@@ -0,0 +1,57 @@
+package camelinaction;
+
+import java.io.File;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.apache.camel.test.junit4.TestSupport.deleteDirectory;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Our first unit test with the Camel Test Kit testing Spring XML routes.
+ * We test the Hello World example of integration kits, which is moving a file.
+ */
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"firststep.xml"})
+public class SpringFirstRunWithTest {
+
+    @Autowired
+    private CamelContext context;
+
+    @Autowired
+    private ProducerTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        // delete directories so we have a clean start
+        deleteDirectory("target/inbox");
+        deleteDirectory("target/outbox");
+    }
+
+    @Test
+    public void testMoveFile() throws Exception {
+        // create a new file in the inbox folder with the name hello.txt and containing Hello World as body
+        template.sendBodyAndHeader("file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
+
+        // wait a while to let the file be moved
+        Thread.sleep(2000);
+
+        // test the file was moved
+        File target = new File("target/outbox/hello.txt");
+        assertTrue("File should have been moved", target.exists());
+
+        // test that its content is correct as well
+        String content = context.getTypeConverter().convertTo(String.class, target);
+        assertEquals("Hello World", content);
+    }
+
+}
