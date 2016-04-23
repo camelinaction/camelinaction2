ad92189b2f6a2a3adf4062fefeca6e8f00dbf93c
Claus Ibsen <claus.ibsen@gmail.com>
2016-04-23 14:43:03 +0200

    Citrus work in progress

---
+++ chapter9/citrus/src/test/java/camelinaction/CitrusIT.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import com.consol.citrus.actions.AbstractTestAction;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.junit.JUnit4CitrusTestDesigner;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+
+public class CitrusIT extends JUnit4CitrusTestDesigner {
+
+    @Test
+    @CitrusTest
+    public void orderStatus() throws Exception {
+        description("Checking order 123 should be in progress");
+
+        action(new AbstractTestAction() {
+            @Override
+            public void doExecute(TestContext testContext) {
+                http().client("statusHttpClient")
+                        .get("/status?id=123")
+                        .contentType("text/xml")
+                        .accept("text/xml");
+            }
+        });
+
+        // JMS receive and response
+        receive("statusEndpoint").payload("<order><id>123</id></order>");
+        send("statusEndpoint").payload("<order><id>123</id><status>In Progress</status></order>");
+
+        action(new AbstractTestAction() {
+            @Override
+            public void doExecute(TestContext testContext) {
+                http().client("statusHttpClient")
+                        .response(HttpStatus.OK)
+                        .payload("<order><id>123</id><status>In Progress</status></order>")
+                        .contentType("text/xml")
+                        .version("HTTP/1.1");
+            }
+        });
+
+    }
+
+
+}
