87f6e5cb7c7957d204d71229e5f9a4fda128da4c
Jonathan Anstey <janstey@gmail.com>
2016-05-01 15:35:42 -0230

    dynamic to example

---
+++ chapter2/ftp-jms/src/test/java/camelinaction/FtpToJMSWithDynamicToTest.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class FtpToJMSWithDynamicToTest extends CamelTestSupport {
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        // create CamelContext
+        CamelContext camelContext = super.createCamelContext();
+        
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory = 
+            new ActiveMQConnectionFactory("vm://localhost");
+        camelContext.addComponent("jms",
+            JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        
+        return camelContext;
+    }
+    
+    @Test
+    public void testPlacingOrders() throws Exception {
+        getMockEndpoint("mock:incomingOrders").expectedMessageCount(1);
+        assertMockEndpointsSatisfied();
+    }
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // load file orders from src/data into the JMS queue
+				from("file:src/data?noop=true")
+					.setHeader("myDest", constant("incomingOrders"))
+					.toD("jms:${header.myDest}");
+                       
+                // test that our route is working
+                from("jms:incomingOrders")
+	                .to("mock:incomingOrders");                
+            }
+        };
+    }
+}
