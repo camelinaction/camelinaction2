4830e310eb1d8a0281e40caf91dfbf02836f2827
Claus Ibsen <claus.ibsen@gmail.com>
2016-07-04 21:48:15 +0200

    Added docs

--- chapter19/custom-tooling-karaf/src/main/java/camelinaction/karaf/DeprecatedComponentsCommand.java
+++ chapter19/custom-tooling-karaf/src/main/java/camelinaction/karaf/DeprecatedComponentsCommand.java
@@ -6,12 +6,17 @@ import camelinaction.DeprecatedValidator;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
+/**
+ * A Karaf command that checks if any Camel application is using deprecated Camel components
+ */
 @Command(scope = "custom", name = "deprecated-components", description = "Lists all deprecated components in use.")
 public class DeprecatedComponentsCommand extends OsgiCommandSupport {
 
     @Override
     protected Object doExecute() throws Exception {
+        // create the validator to use which is from the chapter19-custom-tooling module
         DeprecatedValidator validator = new DeprecatedValidator();
+        // find all the deprecated component names that are in use
         List<String> names = validator.findDeprecatedComponents();
 
         if (names.isEmpty()) {
