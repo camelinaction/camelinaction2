f0cd1b94aebab963d24dc254aa6eadd7b620726e
Claus Ibsen <claus.ibsen@gmail.com>
2016-07-08 19:53:20 +0200

    Chapter 19

---
+++ chapter19/hawtio-debug/src/main/java/camelinaction/MyRouteBuilder.java
@@ -0,0 +1,23 @@
+package camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Notice this route is in Java code, but hawtio can visualize the route in the web browser
+ * and you can add breakpoints and debug the route.
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    public void configure() {
+        from("timer:foo?period=5000")
+            .transform(simple("${random(1000)}"))
+            .choice()
+                .when(simple("${body} > 500"))
+                    .log("High number ${body}")
+                    .to("mock:high")
+                .otherwise()
+                    .log("Low number ${body}")
+                    .to("mock:low");
+    }
+
+}
