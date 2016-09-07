5546d4822508a8f966a14f7331f6525b7db1fd54
Claus Ibsen <claus.ibsen@gmail.com>
2016-09-07 23:00:21 +0200

    Chapter 7

--- chapter7/springboot-rest-camel/ReadMe.md
+++ chapter7/springboot-rest-camel/ReadMe.md
@@ -1,7 +1,7 @@
-chapter7 - spring-boot
-----------------------
+chapter7 - spring-boot-rest-camel
+---------------------------------
 
-Running Spring Boot without Camel
+Running Spring Boot and using Camel from a REST endpoint
 
 ### 7.2.4  Spring Boot with Camel as microservice 
 
@@ -19,6 +19,8 @@ And from a web browser open the following url:
 
 You can also run the example as a fat-jar using: 
 
-    java -jar target/chapter7-springboot-2.0.0.jar
+    java -jar target/chapter7-springboot-rest-camel-2.0.0.jar
     
-    
+Notice this example uses online internet access to lookup your current location.
+This is done using the camel-geocoder component.
+
--- chapter7/springboot-rest-camel/src/main/java/camelinaction/HelloRestController.java
+++ chapter7/springboot-rest-camel/src/main/java/camelinaction/HelloRestController.java
@@ -1,5 +1,7 @@
 package camelinaction;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.FluentProducerTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
 
     /**
+     * Inject Camel producer to use camel-geocoder to find location where we are
+     */
+    @EndpointInject(uri = "geocoder:address:current")
+    private FluentProducerTemplate producer;
+
+    /**
      * HTTP GET method
      */
     @RequestMapping(method = RequestMethod.GET, value = "/hello",
                     produces = "text/plain")
     public String hello() {
-        return "Hello from Spring Boot";
+        // call Camel to find our location, the returned string is in JSon format
+        String where = producer.request(String.class);
+
+        return "Hello from Spring Boot and Camel. We are at: " + where;
     }
 }
 
