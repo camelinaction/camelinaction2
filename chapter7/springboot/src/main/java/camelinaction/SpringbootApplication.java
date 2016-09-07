d52f7dee3110380cf9ccba2613e58f96a4908810
Claus Ibsen <claus.ibsen@gmail.com>
2016-09-07 19:53:26 +0200

    Chapter 7 - Spring Boot

---
+++ chapter7/springboot/src/main/java/camelinaction/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package camelinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
