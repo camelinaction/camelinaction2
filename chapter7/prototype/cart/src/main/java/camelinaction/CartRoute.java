91d8d2851dc7218fd8c1bac961d23071a7ac008c
Claus Ibsen <claus.ibsen@gmail.com>
2016-10-09 20:33:40 +0200

    Chapter 7 - Cart

---
+++ chapter7/prototype/cart/src/main/java/camelinaction/CartRoute.java
@@ -0,0 +1,35 @@
+package camelinaction;
+
+import javax.inject.Singleton;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+
+/**
+ * Shopping Cart REST service using Camel rest-dsl (to be covered in chapter 10)
+ */
+@Singleton
+public class CartRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // use jetty for rest service
+        restConfiguration("jetty").port("{{port}}").contextPath("api")
+                // turn on json binding
+                .bindingMode(RestBindingMode.json)
+                // turn off binding error on empty beans
+                .dataFormatProperty("disableFeatures", "FAIL_ON_EMPTY_BEANS");
+
+        // define the rest service
+        rest("/cart").consumes("application/json").produces("application/json")
+            // get returns List<CartDto>
+            .get().outTypeList(CartDto.class)
+                .to("bean:cart?method=getItems")
+            // get accepts CartDto
+            .post().type(CartDto.class)
+                .to("bean:cart?method=addItem")
+            .delete()
+                .to("bean:cart?method=removeItems");
+    }
+}
