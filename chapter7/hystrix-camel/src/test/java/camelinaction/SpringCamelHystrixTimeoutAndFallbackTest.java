65e52a1606ca3ea0e5dd17952754424168869260
Claus Ibsen <claus.ibsen@gmail.com>
2016-12-29 10:18:30 +0100

    Chapter 7

---
+++ chapter7/hystrix-camel/src/test/java/camelinaction/SpringCamelHystrixTimeoutAndFallbackTest.java
@@ -0,0 +1,32 @@
+package camelinaction;
+
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hystrix using timeout and fallback using XML DSL with Spring
+ */
+public class SpringCamelHystrixTimeoutAndFallbackTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("camelinaction/CamelHystrixTimeoutAndFallbackTest.xml");
+    }
+
+    @Test
+    public void testFast() throws Exception {
+        // this calls the fast route and therefore we get a response
+        Object out = template.requestBody("direct:start", "fast");
+        assertEquals("Fast response", out);
+    }
+
+    @Test
+    public void testSlow() throws Exception {
+        // this calls the slow route and therefore causes a timeout which triggers the fallback
+        Object out = template.requestBody("direct:start", "slow");
+        // TODO: requires Camel 2.18.2 or better
+        // assertEquals("Fallback response", out);
+    }
+}
