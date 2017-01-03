de456b7cc7a3b9dd4d4c7401e34e9ea3e3239f2b
Claus Ibsen <claus.ibsen@gmail.com>
2017-01-03 20:22:07 +0100

    Polished

--- chapter7/prototype2/recommend2/src/main/java/camelinaction/ShoppingCartService.java
+++ chapter7/prototype2/recommend2/src/main/java/camelinaction/ShoppingCartService.java
@@ -1,5 +1,8 @@
 package camelinaction;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,26 +22,15 @@ public class ShoppingCartService {
         // get the current item in the shopping cart associated with the HTTP session id
         LOG.info("Calling cart service {}", cartUrl);
         CartDto[] carts = restTemplate.getForObject(cartUrl, CartDto[].class, id);
-        String cartIds = cartsToCommaString(carts);
+        String cartIds = Arrays.stream(carts)
+            .map(CartDto::toString)
+            .collect(Collectors.joining(","));
         LOG.info("Shopping cart items {}", cartIds);
-
         return cartIds;
     }
 
     public String emptyCart(String cartUrl, String id) {
         return "";
-    }
-
-    private String cartsToCommaString(CartDto[] carts) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < carts.length; i++) {
-            CartDto cart = carts[i];
-            sb.append(cart.getItemId());
-            if (i < carts.length - 1) {
-                sb.append(",");
-            }
-        }
-        return sb.toString();
     }
 
 }
