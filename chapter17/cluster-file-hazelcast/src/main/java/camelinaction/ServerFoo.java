dc4d9c891738d94c5325f50050d1460543301f15
Claus Ibsen <claus.ibsen@gmail.com>
2017-02-19 13:01:30 +0100

    Chapter 17

--- chapter17/cluster-file-hazelcast/src/main/java/camelinaction/ServerFoo.java
+++ chapter17/cluster-file-hazelcast/src/main/java/camelinaction/ServerFoo.java
@@ -1,5 +1,7 @@
 package camelinaction;
 
+import java.util.concurrent.TimeUnit;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.apache.camel.component.hazelcast.policy.HazelcastRoutePolicy;
@@ -22,6 +24,12 @@ public class ServerFoo {
 
         // setup the hazelcast route policy
         HazelcastRoutePolicy routePolicy = new HazelcastRoutePolicy(hz);
+        // the lock names must be same in the foo and bar server
+        routePolicy.setLockMapName("myLock");
+        routePolicy.setLockKey("myLockKey");
+        routePolicy.setLockValue("myLockValue");
+        // attempt to grab lock every 5th second
+        routePolicy.setTryLockTimeout(5, TimeUnit.SECONDS);
 
         main = new Main();
         // bind the hazelcast route policy to the name myPolicy which we refer to from the route
