bfed58b2b09cdbf4bf94d5363733e0398ab2ff4c
Claus Ibsen <claus.ibsen@gmail.com>
2017-02-19 12:22:55 +0100

    Chapter 17

---
+++ chapter17/cluster-file-hazelcast/README.md
@@ -0,0 +1,16 @@
+Chapter 17 - cluster-file-hazelcast
+-----------------------------------
+
+This directory holds an example how to use Hazelcast as clustered route policy in active/passive mode.
+
+### 17.1.2 Clustering files/FTP with Camel
+
+The example requires to run at two JVMs concurrently by starting each Maven goal from each terminal:
+
+    mvn compile exec:java -Pfoo
+    mvn compile exec:java -Pbar
+
+And then copy a bunch of files into the `target/inbox` directory which should let only the master
+JVM consume the files. You can then kill/stop the master JVM and the slave JVM should become the
+new master and consume the files.
+
---
+++ chapter17/cluster-file-hazelcast/src/main/java/camelinaction/ServerBar.java
@@ -0,0 +1,34 @@
+package camelinaction;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.apache.camel.component.hazelcast.policy.HazelcastRoutePolicy;
+import org.apache.camel.main.Main;
+import org.apache.camel.processor.idempotent.hazelcast.HazelcastIdempotentRepository;
+
+public class ServerBar {
+
+    private Main main;
+
+    public static void main(String[] args) throws Exception {
+        ServerBar bar = new ServerBar();
+        bar.boot();
+    }
+
+    public void boot() throws Exception {
+        // create and embed the hazelcast server
+        // (you can use the hazelcast client if you want to connect to external hazelcast server)
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+
+        // setup the hazelcast route policy
+        HazelcastRoutePolicy routePolicy = new HazelcastRoutePolicy(hz);
+
+        main = new Main();
+        // bind the hazelcast route policy to the name myPolicy which we refer to from the route
+        main.bind("myPolicy", routePolicy);
+        // add the route and and let the route be named BAR and use a little delay when processing the files
+        main.addRouteBuilder(new FileConsumerRoute("BAR", 100));
+        main.run();
+    }
+
+}
