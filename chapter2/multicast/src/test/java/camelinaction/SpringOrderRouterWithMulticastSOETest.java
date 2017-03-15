c6fe1919c14582125fd525b877fcde58045b6587
Jonathan Anstey <janstey@gmail.com>
2017-03-15 01:06:20 -0230

    fix test failure

--- chapter2/multicast/src/test/java/camelinaction/SpringOrderRouterWithMulticastSOETest.java
+++ chapter2/multicast/src/test/java/camelinaction/SpringOrderRouterWithMulticastSOETest.java
@@ -8,6 +8,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringOrderRouterWithMulticastSOETest extends CamelSpringTestSupport {
 
     @Override
+    public void setUp() throws Exception {
+        deleteDirectory("activemq-data");
+        super.setUp();
+    }
+    
+    @Override
     protected AbstractXmlApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("camelinaction/SpringOrderRouterWithMulticastSOETest.xml");
     }
