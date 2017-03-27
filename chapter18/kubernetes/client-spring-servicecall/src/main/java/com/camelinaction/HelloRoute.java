a7b02c148766c17db265f5325a37a02701ec5987
Claus Ibsen <claus.ibsen@gmail.com>
2017-03-27 17:50:35 +0200

    Chapter 18 - service call EIP

---
+++ chapter18/kubernetes/client-spring-servicecall/src/main/java/com/camelinaction/HelloRoute.java
@@ -0,0 +1,21 @@
+package com.camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Camel as client that calls the hello service using a timer every 2nd seconds and logs the response
+ */
+@Component
+public class HelloRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period=2000")
+            .serviceCall("helloswarm-kubernetes")
+            .log("${body}");
+    }
+}
+
+
+
