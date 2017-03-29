6710e30f0de560f5e5bbdfe6c593626154e1f1ab
Claus Ibsen <claus.ibsen@gmail.com>
2017-03-29 20:34:05 +0200

    Chapter 21

---
+++ chapter21/rx-java2/src/test/java/camelinaction/CamelConsumeNumbersTest.java
@@ -0,0 +1,42 @@
+package camelinaction;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreams;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreamsService;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class CamelConsumeNumbersTest extends CamelTestSupport {
+
+    @Test
+    public void testConsumeNumbers() throws Exception {
+        CamelReactiveStreamsService rxCamel = CamelReactiveStreams.get(context);
+
+        // use stream engine create a publisher
+        // that just sends 5 numbers, which needs to be sorted
+        // and then each data is send to Camel on the reactive-streams:number endpoint
+        Flowable.just("3", "4", "1", "5", "2")
+            .sorted(String::compareToIgnoreCase)
+            .subscribe(rxCamel.streamSubscriber("numbers", String.class));
+
+        // let it run for 2 seconds
+        Thread.sleep(2000);
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("reactive-streams:numbers")
+                    .log("Got number ${body}");
+            }
+        };
+    }
+}
---
+++ chapter21/rx-java2/src/test/java/camelinaction/CamelFilesTest.java
@@ -0,0 +1,55 @@
+package camelinaction;
+
+import io.reactivex.Flowable;
+import org.apache.camel.Exchange;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreams;
+import org.apache.camel.component.reactive.streams.api.CamelReactiveStreamsService;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class CamelFilesTest extends CamelTestSupport {
+
+    @Test
+    public void testFiles() throws Exception {
+        getMockEndpoint("mock:inbox").expectedMessageCount(4);
+        getMockEndpoint("mock:camel").expectedMessageCount(2);
+
+        CamelReactiveStreamsService rxCamel = CamelReactiveStreams.get(context);
+
+        // use stream engine to subscribe from the publisher
+        // where we filter out the big numbers which is logged
+        Flowable.fromPublisher(rxCamel.from("file:target/inbox"))
+            // call the direct:inbox Camel route from within this flow
+            .doOnNext(e -> rxCamel.to("direct:inbox", e))
+            // filter out files which has Camel in the text
+            .filter(e -> e.getIn().getBody(String.class).contains("Camel"))
+            // let Camel also be subscriber by the endpoint direct:camel
+            .subscribe(rxCamel.subscriber("direct:camel"));
+
+        // create some test files
+        fluentTemplate.to("file:target/inbox").withBody("Hello World").withHeader(Exchange.FILE_NAME, "hello.txt").send();
+        fluentTemplate.to("file:target/inbox").withBody("Hello Camel").withHeader(Exchange.FILE_NAME, "hello2.txt").send();
+        fluentTemplate.to("file:target/inbox").withBody("Bye Camel").withHeader(Exchange.FILE_NAME, "bye.txt").send();
+        fluentTemplate.to("file:target/inbox").withBody("Bye World").withHeader(Exchange.FILE_NAME, "bye2.txt").send();
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:inbox")
+                    .log("Inbox ${header.CamelFileName}")
+                    .wireTap("mock:inbox");
+
+                from("direct:camel")
+                    .log("This is a Camel file ${header.name}")
+                    .to("mock:camel");
+            }
+        };
+    }
+}
