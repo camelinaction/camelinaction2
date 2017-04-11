6441a6441e136c35c711f1569df281ce80949c16
christophd <deppisch@consol.de>
2017-04-11 19:56:01 +0200

    Update to Citrus 2.7.1 also use Spring Java configuration

--- chapter9/citrus/src/test/java/camelinaction/CitrusIT.java
+++ chapter9/citrus/src/test/java/camelinaction/CitrusIT.java
@@ -1,13 +1,19 @@
 package camelinaction;
 
+import com.consol.citrus.actions.AbstractTestAction;
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.condition.AbstractCondition;
 import com.consol.citrus.context.TestContext;
 import com.consol.citrus.dsl.junit.JUnit4CitrusTestDesigner;
+import com.consol.citrus.exceptions.CitrusRuntimeException;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.jms.endpoint.JmsEndpoint;
 import com.consol.citrus.jms.message.JmsMessageHeaders;
-import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Citrus Integration test that test a Camel application.
@@ -19,7 +25,18 @@ import org.springframework.http.HttpStatus;
  * In other words we have Citrus simulating a JMS backend, and as a client as well.
  * Notice how we can setup the test using the citrus designer.
  */
+@ContextConfiguration(classes = EndpointConfig.class)
 public class CitrusIT extends JUnit4CitrusTestDesigner {
+
+    @Autowired
+    private HttpClient statusHttpClient;
+
+    @Autowired
+    private JmsEndpoint statusEndpoint;
+
+    @Autowired
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    private SpringCamelContext orderService;
 
     @Test
     @CitrusTest
@@ -30,7 +47,7 @@ public class CitrusIT extends JUnit4CitrusTestDesigner {
         variable("orderId", "citrus:randomNumber(5)");
 
         // the HTTP client is sending the order
-        http().client("statusHttpClient")
+        http().client(statusHttpClient)
             .send()
                 .get("/status?id=${orderId}")
                 .contentType("text/xml").accept("text/xml")
@@ -42,41 +59,47 @@ public class CitrusIT extends JUnit4CitrusTestDesigner {
         // the Camel application will call a JMS backend so lets use Citrus to simulate this
         // on the JMS queue we expect to receive the following message
         // and capture the JMS correlation ID so we can send back the correct reply message
-        receive("statusEndpoint")
+        receive(statusEndpoint)
                 .payload("<order><id>${orderId}</id></order>")
                 .extractFromHeader(JmsMessageHeaders.CORRELATION_ID, "cid");
 
         // send back the JMS reply message that the order is done
         // and with the correct JMSCorrelationID
-        send("statusEndpoint")
+        send(statusEndpoint)
                 .payload("<order><id>${orderId}</id><status>ON HOLD</status></order>")
                 .header(JmsMessageHeaders.CORRELATION_ID, "${cid}");
 
         // the HTTP client is expected to receive a 200 OK message with the following XML structure
-        http().client("statusHttpClient")
+        http().client(statusHttpClient)
             .receive()
                 .response(HttpStatus.OK)
                 .payload("<order><id>${orderId}</id><status>ON HOLD</status></order>")
                 .contentType("text/xml");
 
         // wait for Camel to shutdown nicely (citrus should have this out of the box in citrus-camel)
-        waitForGracefulShutdown();
+        stopAndWaitForGracefulShutdown();
     }
 
     /**
-     * Wait for graceful shutdown of Camel context before closing the test.
+     * Stop and wait for graceful shutdown of Camel context before closing the test.
      */
-    private void waitForGracefulShutdown() {
+    private void stopAndWaitForGracefulShutdown() {
+        // stop the Camel context with custom test action.
+        action(new AbstractTestAction() {
+            @Override
+            public void doExecute(TestContext context) {
+                try {
+                    orderService.stop();
+                } catch (Exception e) {
+                    throw new CitrusRuntimeException("Failed to stop Camel context");
+                }
+            }
+        });
+
         waitFor().condition(new AbstractCondition() {
             @Override
             public boolean isSatisfied(TestContext context) {
-                try {
-                    context.getApplicationContext().getBean(CamelContext.class).stop();
-                } catch (Exception e) {
-                    return false;
-                }
-
-                return true;
+                return orderService.isStopped();
             }
 
             @Override
---
+++ chapter9/citrus/src/test/java/camelinaction/EndpointConfig.java
@@ -0,0 +1,91 @@
+package camelinaction;
+
+import com.consol.citrus.dsl.endpoint.CitrusEndpoints;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.jms.endpoint.JmsEndpoint;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.TransportConnector;
+import org.apache.activemq.xbean.XBeanBrokerService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
+
+import java.net.URI;
+
+/**
+ * Citrus Spring configuration for JMS backend and Http client components
+ * that are used during the integration test.
+ *
+ * This configuration also imports the Camel application as Spring context.
+ */
+@Configuration
+@ImportResource("classpath:camelinaction/status.xml")
+@PropertySource("classpath:citrus.properties")
+public class EndpointConfig {
+
+    /** ActiveMQ broker url */
+    @Value("${jms.broker.url}")
+    private String jmsBrokerUrl;
+
+    /**
+     * Citrus Http client
+     * do a sync request/reply over HTTP on localhost:8080/order with a GET call and timeout for 60 seconds
+     * @return http client component.
+     */
+    @Bean
+    public HttpClient statusHttpClient() {
+        return CitrusEndpoints.http()
+                .client()
+                .requestUrl("http://localhost:8080/order")
+                .requestMethod(HttpMethod.GET)
+                .contentType("text/xml")
+                .timeout(60000L)
+                .build();
+    }
+
+    /**
+     * Citrus JMS endpoint
+     * do a sync request/reply over JMS on the queue named order.status with a timeout of 10 seconds
+     * @return jms synchronous endpoint component.
+     */
+    @Bean
+    public JmsEndpoint statusEndpoint() {
+        return CitrusEndpoints.jms().synchronous()
+                .connectionFactory(jmsConnectionFactory())
+                .destination("order.status")
+                .timeout(10000L)
+                .build();
+    }
+
+    /**
+     * ActiveMQ jms connection factory depends on embedded message broker.
+     * Setup the JMS connection factory to use for integration test
+     * we will embed an ActiveMQ broker
+     * @return
+     */
+    @Bean
+    @DependsOn("jmsMessageBroker")
+    public ActiveMQConnectionFactory jmsConnectionFactory() {
+        return new ActiveMQConnectionFactory(jmsBrokerUrl);
+    }
+
+    /**
+     * ActiveMQ message broker as embedded service.
+     * @return ActiveMQ message broker service.
+     */
+    @Bean
+    public BrokerService jmsMessageBroker() throws Exception {
+        XBeanBrokerService messageBroker = new XBeanBrokerService ();
+
+        TransportConnector connector = new TransportConnector();
+        connector.setUri(new URI(jmsBrokerUrl));
+        messageBroker.addConnector(connector);
+
+        messageBroker.setStart(true);
+        messageBroker.setUseJmx(false);
+        messageBroker.setPersistent(false);
+
+        return messageBroker;
+    }
+}
