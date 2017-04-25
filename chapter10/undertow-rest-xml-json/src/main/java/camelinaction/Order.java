3e0f8da491050b943540609e9c1d9cf5bbec38f2
Claus Ibsen <claus.ibsen@gmail.com>
2017-04-25 19:40:52 +0200

    Fixed example and made it simpler

--- chapter10/undertow-rest-xml-json/src/main/java/camelinaction/Order.java
+++ chapter10/undertow-rest-xml-json/src/main/java/camelinaction/Order.java
@@ -1,10 +1,21 @@
 package camelinaction;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "order")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Order {
 
+    @XmlElement
     private int id;
+    @XmlElement
     private String partName;
+    @XmlElement
     private int amount;
+    @XmlElement
     private String customerName;
 
     public int getId() {
