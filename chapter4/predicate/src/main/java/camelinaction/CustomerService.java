1780f6c48f3d6556c9c78549c915e2005dfba607
Claus Ibsen <claus.ibsen@gmail.com>
2017-05-09 21:42:13 +0200

    Chapter 4

--- chapter4/predicate/src/main/java/camelinaction/CustomerService.java
+++ chapter4/predicate/src/main/java/camelinaction/CustomerService.java
@@ -23,8 +23,8 @@ public class CustomerService {
      * Notice that we can use any kind of Camel bean parameter binding, so we can bind the message @Body, @Header and so on.
      */
     public boolean isSilver(@JsonPath("$.order.customerId") int customerId) {
-        // its a silver customer if the customer id is < 5000
-        return customerId < 5000;
+        // its a silver customer if the customer id is between 1000 and 4999
+        return customerId >= 1000 && customerId < 5000;
     }
 
 }
