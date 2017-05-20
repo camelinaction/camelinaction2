36b908320985476d9d5defba82c76382d5cfa404
Claus Ibsen <claus.ibsen@gmail.com>
2017-05-20 10:44:36 +0200

    Chapter 18 - spring-boot with health check example

---
+++ chapter18/kubernetes/client-spring-health/src/main/java/com/camelinaction/HelloRoute.java
@@ -0,0 +1,22 @@
+package com.camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Camel as client that calls the hello service using a timer every 2nd seconds and logs the response
+ */
+@Component
+public class HelloRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period=2000")
+            // call the service using {{service:name}}
+            .to("netty4-http://{{service:helloswarm-kubernetes}}?disconnect=true&keepAlive=false")
+            .log("${body}");
+    }
+}
+
+
+
