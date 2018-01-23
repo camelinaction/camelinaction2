5a5db9704179536cd95525c7e7a0695a6a265652
Claus Ibsen <claus.ibsen@gmail.com>
2018-01-23 11:35:46 +0100

    Polished jms client ack example with more details

--- chapter12/riderautoparts-partner/src/main/java/camelinaction/ClientAckBean.java
+++ chapter12/riderautoparts-partner/src/main/java/camelinaction/ClientAckBean.java
@@ -15,6 +15,16 @@ import org.slf4j.LoggerFactory;
  * message (= commit). In case of an exception being thrown or not calling the accept, when the JMS consumer is closed,
  * then the message, from the JMS broker point of view is not accepted, and the message will be redelivered on next attempt.
  * This works in the same way as JMS Transacted Acknowledge Mode does.
+ * <p/>
+ * <b>Important:</b> JMS client acknowledge mode with camel-jms (Spring JMS) does not bring much value
+ * as Spring JMS will automatic acknowledge the message even if you do not call jms.acknowledge()
+ * if the Spring DMLC (message listener, onMessage method) is executed successfully (not exception)
+ * which happens if the Camel message was routed to completion in Camel (eg onComplete is called below)
+ * <p/>
+ * Its recommended not to use JMS client acknowledge mode, but either transacted or auto acknowledge mode
+ * (depending on you need redelivery of failed messages or not). See also more details at:
+ * <a href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/jms/listener/AbstractMessageListenerContainer.html">
+ *     Spring JMS AbstractMessageListenerContainer</a>
  */
 public class ClientAckBean {
 
@@ -26,7 +36,9 @@ public class ClientAckBean {
         final javax.jms.Message jms = exchange.getIn(JmsMessage.class).getJmsMessage();
 
         // add UoW completion to call the JMS client acknowledge when the routing is complete (commit)
-        // if the routing fails with an exception, then acknowledge is not called (rollback), as that
+        // if the routing fails with an exception, then onComplete is not called, and the Spring JMS
+        // message listener will fail as well and trigger a rollback
+
         exchange.addOnCompletion(new SynchronizationAdapter() {
             @Override
             public void onComplete(Exchange exchange) {
