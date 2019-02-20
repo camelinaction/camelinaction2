5449f1eab5e36564e4dc012737126ad6572ea05b
Claus Ibsen <claus.ibsen@gmail.com>
2019-02-20 11:21:52 +0100

    Migrate to Camel 3.0.0-M1 (work in progress)

--- chapter7/cdi/src/main/java/camelinaction/HelloConfiguration.java
+++ chapter7/cdi/src/main/java/camelinaction/HelloConfiguration.java
@@ -32,6 +32,9 @@ public class HelloConfiguration {
      * You can listen for any of the Camel events from org.apache.camel.management.event package.
      */
     void onContextStarted(@Observes CamelEvent.CamelContextStartedEvent event) {
+        // TODO: workaround bug in camel 3.0.0-M1 for camel-cdi / properties
+        event.getContext().addComponent("properties", propertiesComponent());
+
         System.out.println("***************************************");
         System.out.println("* Camel started " + event.getContext().getName());
         System.out.println("***************************************");
