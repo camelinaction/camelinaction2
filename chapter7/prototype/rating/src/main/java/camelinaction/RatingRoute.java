bb5b987fdb3d656e5666ea4790d1f9893abc808f
Claus Ibsen <claus.ibsen@gmail.com>
2021-05-17 12:51:17 +0200

    Upgrade to Camel 3.10.0

--- chapter7/prototype/rating/src/main/java/camelinaction/RatingRoute.java
+++ chapter7/prototype/rating/src/main/java/camelinaction/RatingRoute.java
@@ -10,24 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class RatingRoute extends RouteBuilder {
 
-    @Bean
-    ServletRegistrationBean camelServlet() {
-        // TODO: Camel 2.19 should support this OOTB
-        // use a @Bean to register the Camel servlet which we need to do
-        // because we want to use the camel-servlet component for the Camel REST service
-        ServletRegistrationBean mapping = new ServletRegistrationBean();
-        mapping.setName("CamelServlet");
-        mapping.setLoadOnStartup(1);
-        // CamelHttpTransportServlet is the name of the Camel servlet to use
-        mapping.setServlet(new CamelHttpTransportServlet());
-        mapping.addUrlMappings("/api/*");
-        return mapping;
-    }
-
     @Override
     public void configure() throws Exception {
-
         restConfiguration()
+            .contextPath("api")
             // turn on json binding in rest-dsl
             .bindingMode(RestBindingMode.json);
 
