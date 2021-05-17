3461b69c115b08e58ea77f75f54f6f9db57d2ee0
Claus Ibsen <claus.ibsen@gmail.com>
2021-05-17 14:04:40 +0200

    Upgrade to Camel 3.10.0

--- chapter10/springboot-json/src/test/java/camelinaction/OrderServiceTest.java
+++ chapter10/springboot-json/src/test/java/camelinaction/OrderServiceTest.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // unit test with Camel and spring-boot
 @CamelSpringBootTest
