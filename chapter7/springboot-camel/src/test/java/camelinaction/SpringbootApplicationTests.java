9711228b679367d73278521c7018b534b1af1856
Claus Ibsen <claus.ibsen@gmail.com>
2021-05-17 12:46:33 +0200

    Upgrade to Camel 3.10.0

--- chapter7/springboot-camel/src/test/java/camelinaction/SpringbootApplicationTests.java
+++ chapter7/springboot-camel/src/test/java/camelinaction/SpringbootApplicationTests.java
@@ -1,14 +1,11 @@
 package camelinaction;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Empty test to test that Spring Boot can startup
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
 
